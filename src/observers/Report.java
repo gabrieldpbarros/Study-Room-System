f4d83a9442adca8edf51f54148e9f29c87b1ded2
gabriel <gabrielpanovich@gmail.com>
2026-05-12 20:27:44 -0300

    observers constructors implemented

--- src/observers/Report.java
+++ src/observers/Report.java
@@ -2,6 +2,20 @@ package observers;
 import interfaces.IObserver;
 
 public class Report implements IObserver {
+    private int id;
+
+    public Report(int id) {
+        this.setId(id);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
     @Override
     public void update(String roomType, String period, int id) {
 
