ff81f3ce02f50741a038e350e5438f18aa1eac9d
gabriel <gabrielpanovich@gmail.com>
2026-05-11 22:14:29 -0300

    fields added to the base class

--- src/rooms/BaseSala.java
+++ src/rooms/BaseSala.java
@@ -1,6 +1,13 @@
 package rooms;
+import interfaces.IObserver;
+import java.util.List;
+import java.util.Map;
 
 abstract public class BaseSala {
+    protected int ID;
+    protected List<IObserver> observers;
+    protected Map<String, Map<Integer, IObserver>> schedule;
+
     abstract String getStatus();
     abstract void setStatus(String newStatus);
 }
