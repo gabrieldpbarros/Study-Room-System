2a0459bb12a20f2a781f3f8ecdbd88aa6c20d6ae
gabriel <gabrielpanovich@gmail.com>
2026-05-12 14:30:33 -0300

    schedule class integration

--- src/rooms/BaseRoom.java
+++ src/rooms/BaseRoom.java
@@ -1,31 +1,21 @@
 package rooms;
 import interfaces.IObserver;
 import interfaces.IPolicyStrategy;
+import interfaces.ISchedule;
 import observers.User;
-
+import schedule.Schedule;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 abstract public class BaseRoom {
     protected int id;
     protected List<IObserver> observers;
-    protected Map<String, Map<Integer, User>> schedule;
+    protected ISchedule schedule;
 
     public BaseRoom(int id) {
         this.id = id;
         this.observers = new ArrayList<>();
-        this.schedule = new HashMap<>();
-    }
-
-    private User getOccupantAt(String date, int time) {
-        return this.schedule.get(date).get(time);
-    }
-
-    private void insertReservation(String date, int time, User user) {
-        var dailySchedule = this.schedule.computeIfAbsent(date, k -> new HashMap<>());
-        dailySchedule.put(time, user);
+        this.schedule = new Schedule();
     }
 
     protected void notifyObservers(String roomType) {
@@ -41,36 +31,16 @@ abstract public class BaseRoom {
         this.observers.remove(observer);
     }
 
-    public Map<String, Map<Integer, User>> getSchedule() {
+    public ISchedule getSchedule() {
         return this.schedule;
     }
 
-    public void updateSchedule(Map<String, Map<Integer, User>> newSchedule) {
+    public void updateSchedule(ISchedule newSchedule) {
         this.schedule = newSchedule;
     }
 
     public boolean addReservation(String date, int time, User user, IPolicyStrategy policy) {
-        if (!this.schedule.containsKey(date))
-            // CASO 1: Data sem reservas
-            this.insertReservation(date, time, user);
-        else {
-            // CASO 2: Data com reservas
-            var dateSchedule = this.schedule.get(date);
-            if (!dateSchedule.containsKey(time))
-                // CASO 2.1: Sem reservas no horário
-                this.insertReservation(date, time, user);
-            else {
-                // CASO 2.2: Reserva previamente feita naquele horário
-                var occupant = this.getOccupantAt(date, time);
-                if (policy.canReserve(user, occupant))
-                    // Política permite
-                    this.insertReservation(date, time, user);
-                else
-                    // Política não permite
-                    return false;
-            }
-        }
-        return true;
+        return this.schedule.requestReservation(date, time, user, policy);
     }
 
     abstract String getStatus();
