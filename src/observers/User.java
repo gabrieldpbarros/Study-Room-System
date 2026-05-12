0d0963a2c5a445f06e30b6935e09cb7fedc221db
gabriel <gabrielpanovich@gmail.com>
2026-05-12 01:32:24 -0300

    aesthetic elements added

--- src/observers/User.java
+++ src/observers/User.java
@@ -3,10 +3,16 @@ import interfaces.IObserver;
 
 public class User implements IObserver {
     private int id;
+    private String name;
     private String role;
+
+    public int getId() {
+        return 0;
+    }
 
     @Override
     public void update(String roomType, String period, int id) {
+        System.out.printf("ATUALIZAÇÃO DA RESERVA DE " + this.name + ", ID: " + this.id);
         System.out.printf("Sala do tipo " + roomType + ", ID: " + id + " liberada para o período " + period + ".");
     }
 }
