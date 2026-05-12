74813b80d89eb3627261c2809bc6bb63213c2592
gabriel <gabrielpanovich@gmail.com>
2026-05-11 22:21:16 -0300

    observer methods implemented

--- src/rooms/BaseSala.java
+++ src/rooms/BaseSala.java
@@ -8,6 +8,19 @@ abstract public class BaseSala {
     protected List<IObserver> observers;
     protected Map<String, Map<Integer, IObserver>> schedule;
 
+    public void addObserver(IObserver observer) {
+        this.observers.add(observer);
+    }
+
+    public void removeObserver(IObserver observer) {
+        this.observers.remove(observer);
+    }
+
+    protected void notifyObservers() {
+        for (IObserver observer : this.observers)
+            observer.update();
+    }
+
     abstract String getStatus();
     abstract void setStatus(String newStatus);
 }
