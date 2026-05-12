c8f7fdcb214986987ee4a1d535c819d18c4d503c
gabriel <gabrielpanovich@gmail.com>
2026-05-12 01:33:04 -0300

    interface method added

--- src/policies/FirstInStrategy.java
+++ src/policies/FirstInStrategy.java
@@ -1,9 +1,15 @@
 package policies;
 import interfaces.IPolicyStrategy;
+import observers.User;
 
 public class FirstInStrategy implements IPolicyStrategy {
     @Override
     public void applyPolicy() {
 
+    }
+
+    @Override
+    public boolean canReserve(User reserver, User current) {
+        return false;
     }
 }
--- src/policies/PriorityStrategy.java
+++ src/policies/PriorityStrategy.java
@@ -1,9 +1,15 @@
 package policies;
 import interfaces.IPolicyStrategy;
+import observers.User;
 
 public class PriorityStrategy implements IPolicyStrategy {
     @Override
     public void applyPolicy() {
 
+    }
+
+    @Override
+    public boolean canReserve(User reserver, User current) {
+        return false;
     }
 }
