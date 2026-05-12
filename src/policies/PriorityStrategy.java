149e60133311a8e983ce81b50a25d06168b0f8b6
gabriel <gabrielpanovich@gmail.com>
2026-05-12 16:59:31 -0300

    impl simplification

--- src/policies/PriorityStrategy.java
+++ src/policies/PriorityStrategy.java
@@ -6,7 +6,6 @@ public class PriorityStrategy implements IPolicyStrategy {
     @Override
     public boolean canReserve(User reserver, User current) {
         if (current.getRole().equals("docente")) return false;
-        else if (reserver.getRole().equals("docente")) return true;
-        else return false;
+        else return reserver.getRole().equals("docente");
     }
 }
