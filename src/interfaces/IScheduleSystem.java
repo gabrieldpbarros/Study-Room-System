dd63c2f9228849f9d0d4b4b28a1db87433b9b8ac
Felipi <felipi.martins@unifesp.br>
2026-05-12 20:32:17 -0300

    Implementacao (parcial) do sistema principal

--- src/interfaces/IScheduleSystem.java
+++ src/interfaces/IScheduleSystem.java
@@ -1,11 +1,13 @@
 package interfaces;
 
+import observers.User;
+
 public interface IScheduleSystem {
-    void setPolicy();
+    void setPolicy(IPolicyStrategy newPolicy);
     void newUser();
-    void newRoom();
-    void makeSchedule();
-    void cancelSchedule();
+    void newRoom(int roomId);
+    void makeSchedule(User user, Integer roomId, String date, int time);
+    void cancelSchedule(User user, Integer roomId, String date, int time);
     void getFreeRooms();
     void getBusyRooms();
     void reportRooms();
--- src/system/ScheduleSystem.java
+++ src/system/ScheduleSystem.java
@@ -1,30 +1,69 @@
 package system;
+import interfaces.IPolicyStrategy;
 import interfaces.IScheduleSystem;
+import observers.User;
+import rooms.BaseRoom;
+import rooms.GroupRoom;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static java.lang.System.out;
 
 public class ScheduleSystem implements IScheduleSystem {
+    private IPolicyStrategy systemPolicy;
+    private ArrayList<User> userList = new ArrayList<>();
+    private HashMap<Integer, BaseRoom> roomHash = new HashMap<>();
     @Override
-    public void setPolicy() {
-
+    public void setPolicy(IPolicyStrategy newPolicy) {
+        systemPolicy = newPolicy;
     }
 
     @Override
     public void newUser() {
-
+        int userId = userList.size();
+        User myUser = new User();
+        userList.add(myUser);
+        out.print("Criacao de Sala Realizada Com Sucesso!");
     }
 
     @Override
-    public void newRoom() {
+    public void newRoom(int roomId) {
+        BaseRoom targetRoom = roomHash.get(roomId);
+        if(targetRoom!= null){
+            out.print("Criacao de Sala Fracassou: Duplicata Presente");
+            return;
+        }
 
+        GroupRoom myRoom = new GroupRoom(roomId); // factory entra possivelmente aqui (?)
+        roomHash.put(roomId, myRoom);
+        out.print("Criacao de Sala Realizada Com Sucesso!");
     }
 
     @Override
-    public void makeSchedule() {
+    public void makeSchedule(User user, Integer roomId, String date, int time) {
+        BaseRoom targetRoom = roomHash.get(roomId);
+        if (targetRoom == null) {
+            out.print("Agendamento Fracassou: Sala Inexistente");
+            return;
+        }
 
+        boolean success = targetRoom.addReservation(date, time, user, systemPolicy);
+        if(success) out.print("Agendamento Realizado Com Sucesso!");
+        else out.print("Agendamento Fracassou: Sala Ocupada.");
     }
 
     @Override
-    public void cancelSchedule() {
+    public void cancelSchedule(User user, Integer roomId, String date, int time) {
+        BaseRoom targetRoom = roomHash.get(roomId);
+        if (targetRoom == null) {
+            out.print("Cancelamento Fracassou: Sala Inexistente");
+            return;
+        }
 
+        boolean success = targetRoom.cancelReservation(date, time, user);
+        if(success) out.print("Cancelamento Realizado Com Sucesso!");
+        else out.print("Cancelamento Fracassou: Usuario Nao Possui a Sala");
     }
 
     @Override
