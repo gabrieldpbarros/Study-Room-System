f9cb7e4cc03bd8386f443f13148492b29b2fae8f
gabriel <gabrielpanovich@gmail.com>
2026-05-12 14:30:53 -0300

    schedule class implementation

--- src/schedule/Schedule.java
+++ src/schedule/Schedule.java
@@ -7,45 +7,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Schedule implements ISchedule {
-    private Map<String, Map<Integer, User>> schedule;
+    private Map<String, Map<Integer, User>> calendar;
 
     public Schedule() {
-        this.schedule = new HashMap<>();
+        this.calendar = new HashMap<>();
     }
 
     @Override
     public User getOccupantAt(String date, int time) {
-        return this.schedule.get(date).get(time);
+        if (!calendar.containsKey(date)) return null;
+        return calendar.get(date).get(time);
     }
 
     @Override
     public void insertReservation(String date, int time, User user) {
-        var dailySchedule = this.schedule.computeIfAbsent(date, k -> new HashMap<>());
+        var dailySchedule = this.calendar.computeIfAbsent(date, k -> new HashMap<>());
         dailySchedule.put(time, user);
     }
 
     @Override
     public boolean requestReservation(String date, int time, User user, IPolicyStrategy policy) {
-        if (!this.schedule.containsKey(date))
-            // CASO 1: Data sem reservas
+        var occupant = this.getOccupantAt(date, time);
+
+        if (occupant == null || policy.canReserve(user, occupant)) {
             this.insertReservation(date, time, user);
-        else {
-            // CASO 2: Data com reservas
-            var dateSchedule = this.schedule.get(date);
-            if (!dateSchedule.containsKey(time))
-                // CASO 2.1: Sem reservas no horário
-                this.insertReservation(date, time, user);
-            else {
-                // CASO 2.2: Reserva previamente feita naquele horário
-                var occupant = this.getOccupantAt(date, time);
-                if (policy.canReserve(user, occupant))
-                    // Política permite
-                    this.insertReservation(date, time, user);
-                else
-                    // Política não permite
-                    return false;
-            }
+            return true;
         }
-        return true;
+
+        return false;
     }
 }
