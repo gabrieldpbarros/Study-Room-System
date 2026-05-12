57cef26c26ff1fbd208a6abd1363dcbc6f991215
gabriel <gabrielpanovich@gmail.com>
2026-05-12 14:10:09 -0300

    schedule implementation

--- src/schedule/Schedule.java
+++ src/schedule/Schedule.java
@@ -1,4 +1,51 @@
 package schedule;
+import interfaces.IPolicyStrategy;
+import interfaces.ISchedule;
+import observers.User;
 
-public class Schedule {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Schedule implements ISchedule {
+    private Map<String, Map<Integer, User>> schedule;
+
+    public Schedule() {
+        this.schedule = new HashMap<>();
+    }
+
+    @Override
+    public User getOccupantAt(String date, int time) {
+        return this.schedule.get(date).get(time);
+    }
+
+    @Override
+    public void insertReservation(String date, int time, User user) {
+        var dailySchedule = this.schedule.computeIfAbsent(date, k -> new HashMap<>());
+        dailySchedule.put(time, user);
+    }
+
+    @Override
+    public boolean requestReservation(String date, int time, User user, IPolicyStrategy policy) {
+        if (!this.schedule.containsKey(date))
+            // CASO 1: Data sem reservas
+            this.insertReservation(date, time, user);
+        else {
+            // CASO 2: Data com reservas
+            var dateSchedule = this.schedule.get(date);
+            if (!dateSchedule.containsKey(time))
+                // CASO 2.1: Sem reservas no horário
+                this.insertReservation(date, time, user);
+            else {
+                // CASO 2.2: Reserva previamente feita naquele horário
+                var occupant = this.getOccupantAt(date, time);
+                if (policy.canReserve(user, occupant))
+                    // Política permite
+                    this.insertReservation(date, time, user);
+                else
+                    // Política não permite
+                    return false;
+            }
+        }
+        return true;
+    }
 }
