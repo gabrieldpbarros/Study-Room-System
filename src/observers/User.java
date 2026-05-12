4b2f3bde6d854ffb77022c1fa3456715597755d9
gabriel <gabrielpanovich@gmail.com>
2026-05-11 22:24:29 -0300

    base UML elements implemented

--- src/observers/User.java
+++ src/observers/User.java
@@ -1,4 +1,12 @@
 package observers;
+import interfaces.IObserver;
 
-public class User {
+public class User implements IObserver {
+    private int id;
+    private String role;
+
+    @Override
+    public void update() {
+
+    }
 }
