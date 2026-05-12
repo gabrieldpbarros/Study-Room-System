a30305ebb0658cfb84a7c9e25c2992347ff984e1
gabriel <gabrielpanovich@gmail.com>
2026-05-12 00:07:14 -0300

    base schedule system class

---
+++ src/system/ScheduleSystem.java
@@ -0,0 +1,44 @@
+package system;
+import interfaces.IScheduleSystem;
+
+public class ScheduleSystem implements IScheduleSystem {
+    @Override
+    public void setPolicy() {
+
+    }
+
+    @Override
+    public void newUser() {
+
+    }
+
+    @Override
+    public void newRoom() {
+
+    }
+
+    @Override
+    public void makeSchedule() {
+
+    }
+
+    @Override
+    public void cancelSchedule() {
+
+    }
+
+    @Override
+    public void getFreeRooms() {
+
+    }
+
+    @Override
+    public void getBusyRooms() {
+
+    }
+
+    @Override
+    public void reportRooms() {
+
+    }
+}
