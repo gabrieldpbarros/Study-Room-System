9179f75ea20a8b714068bab80ac8e57cb8901bd1
gabriel <gabrielpanovich@gmail.com>
2026-05-12 22:36:04 -0300

    notification handling interfaces

--- src/interfaces/IObserver.java
+++ src/interfaces/IObserver.java
@@ -1,7 +1,5 @@
 package interfaces;
 
 public interface IObserver {
-    void update(String roomType, int time, int id, String procedure);
-    void update(String roomType, int time, int id, String procedure, IObserver user);
-    void update(String roomType, int time, int id, String procedure, IObserver user, boolean hasPriority);
+    void update(INotificationEvent notification);
 }
