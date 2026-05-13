dc5f0a5ff8a50f4de1f4c682643f28be7f7aee69
gabriel <gabrielpanovich@gmail.com>
2026-05-12 21:19:38 -0300

    notification parsing idea

--- src/rooms/BaseRoom.java
+++ src/rooms/BaseRoom.java
@@ -18,9 +18,53 @@ abstract public class BaseRoom {
         this.schedule = new Schedule();
     }
 
-    protected void notifyObservers(String roomType, String procedure) {
+    protected void notifyObservers(String roomType) {
         for (IObserver observer : this.observers)
-            observer.update(roomType, "placeholder",this.id);
+            observer.update(roomType, "placeholder", this.id);
+    }
+
+    // Sobrecarga 1
+    protected void notifyObservers(String roomType, String procedure, User specificUser) {
+        for (IObserver observer : this.observers) {
+            if (!observer.equals(specificUser)) {
+                observer.update(roomType, "placeholder", this.id);
+            } else {
+                // something
+                if (procedure.equals("RESERVA")) {
+                    observer.update(roomType, "placeholder", this.id);
+                } else if (procedure.equals("CANCELAMENTO")) {
+
+                }
+            }
+        }
+    }
+
+    // Sobrecarga 2
+    protected void notifyObservers(String roomType, String procedure, User previous, User current) {
+        for (IObserver observer : this.observers) {
+            if (observer.equals(previous)) {
+                // lost
+            } else if (observer.equals(current)) {
+                // reserved
+            } else
+                observer.update(roomType, "placeholder", this.id);
+        }
+    }
+
+    protected void parseNotification(User previousOccupant, User newOccupant) {
+        if (previousOccupant == null)
+            this.notifyObservers(this.getRoomType(), "RESERVA", newOccupant);
+        else if (newOccupant == null)
+            this.notifyObservers(this.getRoomType(), "CANCELAMENTO", previousOccupant);
+        else if (!previousOccupant.equals(newOccupant))
+            this.notifyObservers(this.getRoomType(), "SOBRESCRITA", previousOccupant, newOccupant);
+        else
+            this.notifyObservers(this.getRoomType());
+
+    }
+
+    public int getId() {
+        return this.id;
     }
 
     public void addObserver(IObserver observer) {
@@ -42,10 +86,6 @@ abstract public class BaseRoom {
     public boolean addReservation(String date, int time, User user, IPolicyStrategy policy) {
         var currentOccupant = schedule.getOccupantAt(date, time);
         if (schedule.requestReservation(date, time, user, policy)) {
-            if (currentOccupant != null) {
-                this.parseNotification(currentOccupant, user);
-            }
-
             this.schedule.insertReservation(date, time, user);
             this.parseNotification(currentOccupant, user); // currentOccupant == null
             return true;
@@ -56,7 +96,7 @@ abstract public class BaseRoom {
 
     public boolean cancelReservation(String date, int time, User user) {
         var currentOccupant = this.schedule.getOccupantAt(date, time);
-        if (currentOccupant.equals(user)) {
+        if (user.equals(currentOccupant)) {
             this.schedule.removeReservation(date, time, user);
             this.parseNotification(currentOccupant, null);
             return true;
@@ -64,7 +104,5 @@ abstract public class BaseRoom {
         return false;
     }
 
-    abstract void parseNotification(User previousOccupant, User newOccupant);
-    abstract String getStatus();
-    abstract void setStatus(String newStatus);
+    abstract String getRoomType();
 }
