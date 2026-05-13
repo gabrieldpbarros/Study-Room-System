1831784f11c5545247b63e02988744c5a466ffa8
gabriel <gabrielpanovich@gmail.com>
2026-05-12 21:43:21 -0300

    update overloading

--- src/interfaces/IObserver.java
+++ src/interfaces/IObserver.java
@@ -1,5 +1,7 @@
 package interfaces;
 
 public interface IObserver {
-    void update(String roomType, String period, int id);
+    void update(String roomType, int time, int id, String procedure);
+    void update(String roomType, int time, int id, String procedure, IObserver user);
+    void update(String roomType, int time, int id, String procedure, IObserver user, boolean hasPriority);
 }
