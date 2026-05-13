d8ef21952a476a12bb74a1aeffaf7cea7e68d289
gabriel <gabrielpanovich@gmail.com>
2026-05-12 22:35:20 -0300

    observers implementation

--- src/observers/User.java
+++ src/observers/User.java
@@ -1,4 +1,6 @@
 package observers;
+import events.NotificationEvent;
+import interfaces.INotificationEvent;
 import interfaces.IObserver;
 
 public class User implements IObserver {
@@ -22,9 +24,45 @@ public class User implements IObserver {
 
     public String getRole() { return this.role; }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
-    public void update(String roomType, String period, int id) {
-        System.out.printf("ATUALIZAÇÃO DA RESERVA DE " + this.name + ", ID: " + this.id);
-        System.out.printf("Sala do tipo " + roomType + ", ID: " + id + " liberada para o período " + period + ".");
+    public void update(INotificationEvent event) {
+        if (event instanceof NotificationEvent roomEvent) {
+            boolean isPrevious = this.equals(roomEvent.getPreviousOccupant());
+            boolean isNew = this.equals(roomEvent.getNewOccupant());
+            String roomInfo = roomEvent.getRoomType() + " (ID: " + roomEvent.getTargetId() + ")";
+            String dateTime = "dia " + roomEvent.getDate() + " às " + roomEvent.getTime() + "h";
+
+            switch (roomEvent.getEventType()) {
+                case "RESERVA":
+                    if (isNew)
+                        System.out.printf("[SISTEMA para %s]: Sucesso! Você reservou a %s para o %s.\n", this.name, roomInfo, dateTime);
+                    else
+                        System.out.printf("[ALERTA GERAL para %s]: A %s foi reservada no %s.\n", this.name, roomInfo, dateTime);
+                    break;
+
+                case "CANCELAMENTO":
+                    if (isPrevious)
+                        System.out.printf("[SISTEMA para %s]: Confirmação. Sua reserva na %s para o %s foi cancelada.\n", this.name, roomInfo, dateTime);
+                    else
+                        System.out.printf("[ALERTA GERAL para %s]: Um horário na %s foi LIBERADO no %s!\n", this.name, roomInfo, dateTime);
+                    break;
+
+                case "SOBRESCRITA":
+                    if (isPrevious)
+                        System.out.printf("[AVISO URGENTE para %s]: Sua reserva na %s para o %s foi CANCELADA por prioridade de Docente.\n", this.name, roomInfo, dateTime);
+                    else if (isNew)
+                        System.out.printf("[SISTEMA para %s]: Sucesso! Você utilizou sua prioridade e reservou a %s para o %s.\n", this.name, roomInfo, dateTime);
+                    else
+                        System.out.printf("[ALERTA GERAL para %s]: Ocorreu uma sobrescrita por prioridade na %s no %s.\n", this.name, roomInfo, dateTime);
+                    break;
+
+                default:
+                    System.out.println("[SISTEMA]: Notificação desconhecida recebida.");
+            }
+        }
     }
 }
